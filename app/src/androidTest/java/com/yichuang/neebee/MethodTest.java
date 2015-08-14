4ed8540eb7998cba47e2d093dff30adcb8aabe8d
ruixiang_cao <crx182173929@gmail.com>
2015-08-14 18:19:43 +0800

    更改包名
    更改包名

---
+++ app/src/androidTest/java/com/yichuang/neebee/MethodTest.java
@@ -0,0 +1,13 @@
+package com.yichuang.neebee;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by cao_ruixiang on 15/8/14.
+ */
+public class MethodTest extends InstrumentationTestCase {
+    public void test() {
+        System.out.println("------------------");
+        assertEquals(2, 3);
+    }
+}
